c8520b2ca263b64862798eaca1efb21f81179e38
Muhammad Baqir <hzindabad44@gmail.com>
2026-08-30 16:57:52 +0200

    Add Prometheus metrics monitoring endpoint

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
+import uvicorn
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.api.endpoints import router as api_router
 
 app = FastAPI(title="Customer Churn Analytics API", version="1.0.0")
 
-# This sets the URL path to /api/v1/churn/predict
+# Instrument FastAPI app and expose Prometheus /metrics endpoint
+Instrumentator().instrument(app).expose(app)
+
+# Include API routes
 app.include_router(api_router, prefix="/api/v1/churn", tags=["churn"])
 
 if __name__ == "__main__":
-    import uvicorn
-
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
